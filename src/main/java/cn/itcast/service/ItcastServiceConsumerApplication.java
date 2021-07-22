be878cff775fa572807968fcaa0a115a5d567bd5
zhangyudong <977872239@qq.com>
2021-07-23 00:58:59 +0800

    1.服务消费方的搭建

---
+++ src/main/java/cn/itcast/service/ItcastServiceConsumerApplication.java
@@ -0,0 +1,19 @@
+package cn.itcast.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ItcastServiceConsumerApplication {
+
+    @Bean//做远程的调用 ,在上一课
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(ItcastServiceConsumerApplication.class, args);
+    }
+
+}
