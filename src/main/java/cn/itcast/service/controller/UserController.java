0fcacfff483cc049194d031a753e8eeae1ee74f9
YuDongZhang <977872239@qq.com>
2021-07-23 17:48:01 +0800

    3,获取服务端动态配置ip,浏览器测试成功http://localhost/consumer/user?id=1

--- src/main/java/cn/itcast/service/controller/UserController.java
+++ src/main/java/cn/itcast/service/controller/UserController.java
@@ -2,6 +2,8 @@ package cn.itcast.service.controller;
 
 import cn.itcast.service.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,16 +11,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("consumer/user")
 public class UserController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;//包含了拉取的所有的服务信息
+
     @GetMapping
     @ResponseBody
     public User queryUserById(@RequestParam("id") Long id){
-        User user = this.restTemplate.getForObject("http://localhost:8081/user/" + id, User.class);
+        List<ServiceInstance> instances = discoveryClient.getInstances("service-provider");
+        ServiceInstance instance = instances.get(0);
+        User user = this.restTemplate.getForObject("http://"+instance.getHost()+":"+instance.getPort()+"/user/" + id, User.class);
         return user;
     }
 
